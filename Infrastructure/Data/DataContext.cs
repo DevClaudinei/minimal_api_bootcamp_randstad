55dc27d0856a291571a04ea00ab26971296fc1b2
Claudinei Santos <neibaianinho@hotmail.com>
2024-09-26 22:47:44 -0300

    feat: criacao do contexto

---
+++ Infrastructure/Data/DataContext.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using minimal_api.Domain.Models.Models;
+
+namespace minimal_api.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Administrator> Administrators { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Administrator>().HasData(
+                new Administrator {
+                    Id = 1,
+                    Email = "admin@bluphy.com.br",
+                    Password = "B1l2u3p4h5y6",
+                    Profile = "Admin"
+                }
+            );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = _configuration.GetConnectionString("mysql");
+
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+}
